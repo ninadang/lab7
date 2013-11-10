7ff67feeede6b0b5491763e1ec0021e9b42f32be
Nina Dang <ninadangt@gmail.com>
2013-11-09 18:38:51 -0800

    JS file lab

--- js/earthquakes.js
+++ js/earthquakes.js
@@ -30,3 +30,58 @@ $(document).ajaxError(function(event, jqXHR, err){
     alert('Problem obtaining data: ' + jqXHR.statusText);
 });
 
+$(function(){
+	getQuakes();
+
+}); //on ready function
+
+function getQuakes(){
+	$.getJSON(gov.usgs.quakesUrl, function(quakes){
+    	gov.usgs.quakes = quakes;
+    	
+    	$('.message').html('Displaying ' + quakes.length + ' earthquakes');
+    	
+    	gov.usgs.quakesMap = new google.maps.Map($('.map-container')[0], {
+		    center: new google.maps.LatLng(0,0),        
+		    zoom: 2,                                    
+		    mapTypeId: google.maps.MapTypeId.TERRAIN,   
+		    streetViewControl: false                    
+		});
+
+		addQuakeMarkers(gov.usgs.quakes, gov.usgs.quakesMap);
+	});
+} //getQuakes()
+
+function addQuakeMarkers(quakes, map){
+	var quake;      
+    var idx;   
+    var infoWindow;
+
+    for (idx = 0; idx < quakes.length; ++idx) {
+        quake = quakes[idx];
+        if(quake.location){
+        	quake.mapMarker = new google.maps.Marker({
+    			map: map,
+    			position: new google.maps.LatLng(quake.location.latitude, quake.location.longitude)
+			});
+		} //check location
+
+		infoWindow = new google.maps.InfoWindow({
+    		content: new Date(quake.datetime).toLocaleString() + 
+                ': magnitude ' + quake.magnitude + ' at depth of ' + 
+                 quake.depth + ' meters'
+		}); //create info window
+
+		registerInfoWindow(map, quake.mapMarker, infoWindow);
+    } //for each quake   
+} //addQuakeMarkers()
+
+function registerInfoWindow(map, marker, infoWindow) {
+    google.maps.event.addListener(marker, 'click', function(){
+    	if(gov.usgs.iw){
+			gov.usgs.iw.close();
+		}
+    	gov.usgs.iw = infoWindow;
+        infoWindow.open(map, marker);
+    });               
+} //registerInfoWindow()
